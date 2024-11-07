bfaf23afb09c8ad2ac3e89a230d77788688863bb
nicolasfavier <niko.favier@gmail.com>
2024-11-07 11:09:50 +0100

    fix ui thread

--- src/main/kotlin/com/takima/shortcutlearner/view/ShortcutLearnerToolWindowFactory.kt
+++ src/main/kotlin/com/takima/shortcutlearner/view/ShortcutLearnerToolWindowFactory.kt
@@ -1,5 +1,6 @@
 package com.takima.shortcutlearner.view
 
+import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.wm.ToolWindow
 import com.intellij.openapi.wm.ToolWindowFactory
@@ -9,12 +10,14 @@ private const val SHORTCUTS_WINDOW_NAME = "Shortcuts"
 class ShortcutLearnerToolWindowFactory : ToolWindowFactory {
     override fun createToolWindowContent(project: Project, toolWindow: ToolWindow) {
         val shortcutPanel = ShortcutLearnerPanel(project)
-        toolWindow.contentManager.addContent(
-            toolWindow.contentManager.factory.createContent(
-                shortcutPanel,
-                SHORTCUTS_WINDOW_NAME,
-                false
+        ApplicationManager.getApplication().invokeLater {
+            toolWindow.contentManager.addContent(
+                toolWindow.contentManager.factory.createContent(
+                    shortcutPanel,
+                    SHORTCUTS_WINDOW_NAME,
+                    false
+                )
             )
-        )
+        }
     }
 }
